a6eb783637dd4897a7ccb0e145f287b87d0599e9
samedfadavi <samed.fadavi@gmail.com>
2026-01-14 23:45:31 -0800

    Add Test Section In Pipeline

--- PmServiceNetCoreTestProject/obj/Debug/net8.0/PmServiceNetCore.Tests.AssemblyInfo.cs
+++ PmServiceNetCoreTestProject/obj/Debug/net8.0/PmServiceNetCore.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PmServiceNetCore.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc57301faafac8c9e0c4333ee42e00f1639e7163")]
 [assembly: System.Reflection.AssemblyProductAttribute("PmServiceNetCore.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PmServiceNetCore.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
